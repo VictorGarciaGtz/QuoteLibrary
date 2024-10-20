7ea9b79b09d2ad92aae5b8384c0a40efab3c11f6
Victor Garcia <victor.garciagutierrez.itcm@gmail.com>
2024-10-19 19:44:05 -0600

    (Id ####) Pruebas unitarias - Se realizan las pruebas para TypesQuotesService.

--- QuoteLibrary.Application.Tests/Services/TypeQuotesServiceTests.cs
+++ QuoteLibrary.Application.Tests/Services/TypeQuotesServiceTests.cs
@@ -49,5 +49,82 @@ namespace QuoteLibrary.Application.Tests.Services
 
         }
 
+        [Fact]
+        public async Task GetAllTypesQuotesAsync_ReturnsListOfTypesQuotesDto()
+        {
+            //Arrange
+            var typesQuotesList = new List<TypesQuotes>
+            {
+                new TypesQuotes { Id = 1, Name = "Quote 1", CreationDate = DateTime.Now },
+                new TypesQuotes { Id = 2, Name = "Quote 2", CreationDate = DateTime.Now },
+                new TypesQuotes { Id = 3, Name = "Quote 3", CreationDate = DateTime.Now }
+            };
+
+            _mockTypeQuotesRepository.Setup(repo => repo.GetAllTypesQuotesAsync()).ReturnsAsync(typesQuotesList);
+
+            //Act
+            var result = await _typeQuotesService.GetAllTypesQuotesAsync();
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.True(result.Count() == 3);
+        }
+
+        [Fact]
+        public async Task CreateTypesQuotesAsync_AddNewTypesQuotes_ReturnsNewId()
+        {
+            //Arrange
+            var typeQuotesName = "Quotes 5";
+
+            _mockTypeQuotesRepository.Setup(repo => repo.CreateTypesQuotesAsync(It.IsAny<TypesQuotes>()))
+                                        .ReturnsAsync(1);
+
+            //Act
+            var result = await _typeQuotesService.CreateTypesQuotesAsync(typeQuotesName);
+
+
+            //Assert
+            Assert.True(result == 1);          
+        }
+
+        [Fact]
+        public async Task UpdateTypesQuotesAsync_UpdateTypesQuotes_ReturnBool()
+        {
+            //Arrange
+            var typeQuotesId = 1;
+            var typeQuotes = new TypesQuotes
+            {
+                Id = typeQuotesId,
+                Name = "Pelicula",
+                CreationDate = new DateTime(2024, 10, 3),
+                ModificationDate = new DateTime(2024, 10, 6)
+            };
+
+            _mockTypeQuotesRepository.Setup(repo => repo.GetTypesQuotesByIdAsync(typeQuotesId)).ReturnsAsync(typeQuotes);
+
+            _mockTypeQuotesRepository.Setup(repo => repo.UpdateTypesQuotesAsync(It.IsAny<TypesQuotes>())).ReturnsAsync(true);
+
+            //Act
+            var result = await _typeQuotesService.UpdateTypesQuotesAsync(1, "Quotes 7");
+
+            //Assert
+            Assert.True(result == true);
+        }
+
+        [Fact]
+        public async Task DeleteTypesQuotesAsync_DeleteTypesQuotes_ReturnBool()
+        {
+            //Arrange
+            var typeQuotesId = 1;
+
+            _mockTypeQuotesRepository.Setup(repo => repo.DeleteTypesQuotesAsync(typeQuotesId)).ReturnsAsync(true);
+
+            //Act
+            var result = await _typeQuotesService.DeleteTypesQuotesAsync(typeQuotesId);
+
+            //Assert
+            Assert.True(result == true);
+
+        }
     }
 }
